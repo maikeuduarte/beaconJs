c8b548ce50df2359d8febd3ec4eb61634c47ac53
Maikeu Duarte <maikeuduarte@gmail.com>
2018-05-05 14:14:54 -0300

    teste

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,7 +28,7 @@ angular.module('starter.controllers', [])
       });
 
       $cordovaBeacon.startMonitoringForRegion($cordovaBeacon.createBeaconRegion("cec1db049d41", "B9407F30-F5F8-466E-AFF9-25556B57FE6D"));
-
+      $rootScope.apply();
     });
 
   })
